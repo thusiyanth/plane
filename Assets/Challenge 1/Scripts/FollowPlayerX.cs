8635d0aac00ed31a3ae367526c8d1449ecc6c6f7
HP <thusiyanth019@gmail.com>
2023-03-10 15:14:48 +0530

    first commit

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    // unity all was using float values
+    private Vector3 offset= new Vector3(14.25f, 0.49f, 0.21f);
+
+    // Start is called before the first frame update 
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
---
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed=300;
+    private float turnSpeed = 50.0f;
+    public float rotationSpeed =350;
+    public float verticalInput;
+    private float horizontalInput;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        // Move the vehicle  horizontal
+       horizontalInput = Input.GetAxis("Horizontal");
+
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
